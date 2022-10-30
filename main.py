eec0851af528f70a0d2a1b800e18725f24d56278
noezee <99247624+noezee@users.noreply.github.com>
2022-10-30 22:15:07 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, redirect, url_for
 from config import host, user, password, database
-import psycopg2, requests, bcrypt
+import psycopg2, requests
 from flask_sqlalchemy import SQLAlchemy
 from models import User
 from db import db
@@ -11,9 +11,13 @@ from flask_login import LoginManager, UserMixin, login_user, login_required, log
 from flask_bootstrap import Bootstrap
 from werkzeug.security import generate_password_hash, check_password_hash
 
+
+
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'Secretkey'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////mnt/c/Users/antho/Documents/login-example/database.db'
+app.config['SECRET_KEY'] = 'Secret'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///user.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
 bootstrap = Bootstrap(app)
 db = SQLAlchemy(app)
 login_manager = LoginManager()
@@ -24,27 +28,33 @@ login_manager.login_view = 'login'
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(15), unique=True)
-    email = db.Column(db.String(50), unique=True)
     password = db.Column(db.String(80))
+
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
+
 
 class LoginForm(FlaskForm):
     username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
     password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
     remember = BooleanField('remember me')
 
-class RegisterForm(FlaskForm):
 
+class RegisterForm(FlaskForm):
     username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
     password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
+
+
+with app.app_context():
+    db.create_all()
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -58,7 +68,7 @@ def login():
                 return redirect(url_for('dashboard'))
 
         return '<h1>Invalid username or password</h1>'
-        #return '<h1>' + form.username.data + ' ' + form.password.data + '</h1>'
+
 
     return render_template('login.html', form=form)
 
@@ -68,15 +78,19 @@ def signup():
 
     if form.validate_on_submit():
         hashed_password = generate_password_hash(form.password.data, method='sha256')
-        new_user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        new_user = User(username=form.username.data, password=hashed_password)
         db.session.add(new_user)
         db.session.commit()
 
         return '<h1>New user has been created!</h1>'
-        #return '<h1>' + form.username.data + ' ' + form.email.data + ' ' + form.password.data + '</h1>'
+
 
     return render_template('signup.html', form=form)
 
+@app.route('/dashboard')
+@login_required
+def dashboard():
+    return render_template('dashboard.html', name=current_user.username)
 
 
 @app.route('/logout')
@@ -85,17 +99,18 @@ def logout():
     logout_user()
     return redirect(url_for('index'))
 
+
 if __name__ == '__main__':
     app.run(debug=True)
-@app.route("/")
-def hello_world():
 
+
+@app.route("/")
+def home():
     return render_template('index.html')
 
+
 if __name__ == ('__main__'):
-   app.run (debug=True)
-
-
+    app.run(debug=True)
 
 url = "https://solana-gateway.moralis.io/nft/mainnet/2camF6sXuq4eQ2h2815xiA2r3BvoYNQbFztbLNymiwsq/metadata"
 headers = {
@@ -109,8 +124,6 @@ response = requests.get(url, headers=headers)
 
 print(response.text)
 
-
-
 connection = psycopg2.connect(database=database, user=user, password=password, host=host)
 
 cursor = connection.cursor()
@@ -122,4 +135,3 @@ cursor.execute("""SELECT * from metaplex""")
 record = cursor.fetchall()
 
 print("Data from Database:- ", record)
-
